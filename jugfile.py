d2be5cb49cccb2717b3bebd9003d228902815fff
Peter Shaffery <pesh5067@shas0137.rc.int.colorado.edu>
2018-07-13 15:13:59 -0600

    jugfil

---
+++ jugfile.py
@@ -0,0 +1,18 @@
+import jug
+from itertools import *
+
+@TaskGenerator
+def decrypt(prefix, suffix_size):
+    res = []
+    for p in product(letters, repeat=suffix_size):
+        text = decode(ciphertext, np.concatenate([prefix, p]))
+        if isgood(text):
+            passwd = "".join(map(chr, p))
+            res.append((passwd, text))
+    return res
+
+@TaskGenerator
+def join(partials):
+    return list(chain(*partials))
+
+fullresults = join([decrypt([let], 4) for let in letters])
