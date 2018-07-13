bccabd2c1befc234dee2e43375031323fcf32ded
pesh5067 <pesh5067@bromwich.colorado.edu>
2018-07-13 09:48:10 -0600

    prepping sampler for bromwich GPU

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import pickle
 import numpy as np
 import numpy.random as npr
+from numba import vectorize
+from numba import float64
 
 import prog_bar
 import epidemic
@@ -25,18 +27,23 @@ def generateSyntheticData( n_mems, parms, time_data ):
 
     return (t_points, y)
 
-def expectedLikelihood( obs, n_mems, parms, aug_data_n_draws, thin = None ):
-    t_points, y = obs                      
-    beta, gamma, init_probs, rho = parms
-    draws = int( aug_data_n_draws )
-
+def expectedLikelihood( beta, gamma  ):
+#    prob_inf = 10. / n_mems
+#    init_probs = [ 1. - prob_inf, prob_inf, 0. ]
+#    
+#    t_points, y = obs                      
+#    beta, gamma, init_probs, rho = parms
+#    draws = int( aug_data_n_draws )
+#
     chain = sampler.da_sampler( y, t_points, n_mems, beta, gamma, init_probs, rho)
+    print('init...')
     chain.initialize()
+    print('...completed')
 
     like_samps = np.ones( draws, dtype=np.float64 )
     
-    bar = prog_bar.percent_bar( draws )
-    bar.set()
+    #bar = prog_bar.percent_bar( draws )
+    #bar.set()
     for i in range(draws):
         try:
             chain.update( npr.choice( range(n_mems) ) )
@@ -46,43 +53,65 @@ def expectedLikelihood( obs, n_mems, parms, aug_data_n_draws, thin = None ):
             break
         
         like_samps[i] = chain.full_likelihood()
-        bar.step()
+        #bar.step()
 
-    return ( np.mean( like_samps ), chain )
+    return np.mean( like_samps )
 
 
 def main():
     time_start = time.time()
 
+    global n_mems
     n_mems = int(1e2)
 
+    global prob_inf
+    global init_probs
     prob_inf = 10. / n_mems
     init_probs = [ 1. - prob_inf, prob_inf, 0. ]
     
     beta_true = beta = .2
     gamma_true = gamma = 2.
+
+    global rho
     rho = .4
     
     t_start = 0
     delta_t = 1e-3
     t_steps = 100
 
+    global t_points
+    global y
     t_points, y = generateSyntheticData( n_mems, [beta, gamma, init_probs, rho], [t_start, delta_t, t_steps] )
 
-    exp_like, chain = expectedLikelihood( [t_points, y], n_mems, [beta, gamma, init_probs, rho], 1000 )
-    
-    exp_like_surface = [[beta,gamma,exp_like]]
+    global draws
+    draws = 1000
+
+    global exp_like
+
+    betas = np.linspace(0,10,1000)
+    exp_like_surface = np.array([[0,0,0]]*len(betas))
+
+    for i in range(len(betas)):
+        exp_like = expectedLikelihood( betas[i], gamma )
+        exp_like_surface[i] = [betas[i],gamma,exp_like]
+        sys.stdout.write(str(i))
+        sys.stdout.flush()
+
+        if i % 20 == 0:
+            print i
+            with open('like_surface.pickle','wb') as handle:
+                pickle.dump( exp_like_surface, handle ) 
+
+        else:
+            pass
     
     time_end = time.time()
     print( 'run time was ' + str(time_end-time_start) )
 
-    return chain
 
 if __name__=='__main__':
-    import scipy.linalg as spla
-    chain = main()
-    test = chain
-
+    main()
+    
 
 # some handy auxillary funcs that I only use sometimes
 def time_autocorr(x,lag):
