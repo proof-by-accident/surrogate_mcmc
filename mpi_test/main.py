6102903194146b68f8ebfaaadc22c9ebeabc9606
Peter Shaffery <pesh5067@shas0137.rc.int.colorado.edu>
2018-07-20 09:38:18 -0600

    second run complete

--- mpi_test/main.py
+++ mpi_test/main.py
@@ -6,20 +6,25 @@ comm = MPI.COMM_WORLD
 nproc = comm.Get_size()
 iproc = comm.Get_rank()
 
-parms_array_send = np.empty( [nproc**2,2], dtype=np.float64 )
-parms_array = np.empty([nproc,2], dtype=np.float64)
+#parms_array_send = np.empty( [nproc**2,2], dtype=np.float64 )
+#parms_array = np.empty([nproc,2], dtype=np.float64)
+
+x = y = np.empty(nproc, dtype=np.float64)
+X = Y = np.empty([nproc, nproc], dtype=np.float64) 
 
 if iproc == 0:
         x = y = np.linspace(0,1,nproc)
         X,Y = np.meshgrid(x,y)
-        parms_array_send = np.array([ [a,b] for a,b in zip([x for x_list in X for x in x_list], [y for y_list in Y for y in y_list]) ])
+        #parms_array_send = np.array([ [a,b] for a,b in zip([x for x_list in X for x in x_list], [y for y_list in Y for y in y_list]) ])
 else:
         pass
 
-comm.Scatter( parms_array_send, parms_array, root=0)
+#comm.Scatter( parms_array_send, parms_array, root=0)
+comm.Scatter(X,x,root=0)
+comm.Scatter(Y,y,root=0)
 
 if iproc !=0 :
-        print iproc, parms_array
+        print iproc, x==y, x
 
 else:
         pass
