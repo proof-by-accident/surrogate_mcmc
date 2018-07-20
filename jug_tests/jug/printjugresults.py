c126f1dba76f85078f0e2b52607177710d8c3ffe
Peter Shaffery <pesh5067@shas0137.rc.int.colorado.edu>
2018-07-20 11:04:27 -0600

    housekeeping

--- jug_tests/jug/printjugresults.py
+++
@@ -1,8 +0,0 @@
-import jug
-jug.init('jugfile.py', 'jugfile.jugdata')
-import jugfile
-
-
-results = jug.task.value(jugfile.fullresults)
-for p, t in results:
-    print("%s\n\n    Password was '%s'" % (t, p))
